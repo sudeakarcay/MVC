59a46a57d268628a86c839ae8a4c4f4e4e8aef6e
Sude Akarçay <akarcaysude2@gmail.com>
2024-10-06 15:49:49 +0300

    Add project files.

---
+++ BLL/Models/StudentModel.cs
@@ -0,0 +1,17 @@
+﻿using BLL.DAL;
+
+namespace BLL.Models
+{
+    public class StudentModel
+    {
+        public Student Record { get; set; }
+        
+        public String Name => Record.Name;
+
+        public String Surname => Record.Surname;
+
+        public DateTime? BirthDate => Record.BirthDate;
+
+        public decimal? Gpa => Record.Gpa;
+    }
+}
